39b9dbd4d130fb526aefc2ece6158b07f7ce17b2
Moiz-stack <moiz.workmail@gmail.com>
2024-08-30 20:04:21 -0700

    Initial commit

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+function useCurrencyInfo(currecny) {
+
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currecny}.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currecny]));
+    }, [currecny]);
+    
+    return data
+}
+
+export default useCurrencyInfo
